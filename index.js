90c4303d9aaf6983e72662bc6c287cc91aa8115c
michaelslain <misha.slain@icloud.com>
2020-12-29 17:35:58 -0800

    :rocket:

---
+++ index.js
@@ -0,0 +1,133 @@
+const operators = ['+', '-', '?', '*', '%', '/', '<', '>', '=', '!']
+const brackets = ['(', ')', '[', ']', '{', '}']
+const quotes = ['"', `'`, '`']
+const declarations = [
+    'function',
+    'var',
+    'let',
+    'if',
+    'else',
+    'switch',
+    'case',
+    'break',
+    'const',
+    'return',
+    'import',
+    'from',
+    'export',
+    'default',
+    'continue',
+]
+
+export function color(string, color) {
+    return `<font color="${color}">${string}</font>`
+}
+
+function isNumber(num) {
+    return !Number.isNaN(Number(num)) && num !== ' ' && num !== '\n'
+}
+
+export function highlight(tokens) {
+    let code = ''
+
+    tokens.forEach(({ string, type, quote }) => {
+        switch (type) {
+            case 'declaration':
+                code += color(string, 'cornflowerblue')
+                break
+            case 'string':
+                code += color(quote + string + quote, 'lime')
+                break
+            case 'operator':
+                code += color(string, 'violet')
+                break
+            case 'number':
+                code += color(string, 'orange')
+                break
+            case 'bracket':
+                code += color(string, 'red')
+                break
+            case 'normal':
+                code += string
+                break
+        }
+    })
+
+    return code
+}
+
+export function optimize(tokens, lastToken = {}) {
+    if (tokens.length <= 1) return tokens
+
+    if (lastToken.type === 'normal' && tokens[0].type === 'normal') {
+        lastToken.string += tokens[0].string
+        return optimize(tokens.slice(1), lastToken)
+    }
+
+    return [tokens[0]].concat(optimize(tokens.slice(1), tokens[0]))
+}
+
+export function tokenize(code) {
+    code = code.trim()
+    let tokens = []
+
+    for (let i = 0; i < code.length; ) {
+        const char = code[i]
+
+        if (quotes.includes(char)) {
+            const quote = char
+
+            let j
+            for (j = i + 1; j < code.length; j++) if (code[j] === quote) break
+
+            tokens.push({
+                string: code.substring(i + 1, j),
+                type: 'string',
+                quote,
+            })
+            i += j - i + 1
+            continue
+        }
+        let foundDeclaration
+        declarations.find(declaration => {
+            const prefixes = brackets.concat([' ', '\n', ';'])
+
+            const snippet = code.substring(i, i + declaration.length)
+            if (
+                snippet === declaration &&
+                (prefixes.includes(code[i - 1]) || i === 0)
+            ) {
+                tokens.push({ string: declaration, type: 'declaration' })
+                i += declaration.length
+                foundDeclaration = true
+                return true
+            }
+
+            return false
+        })
+        if (foundDeclaration) continue
+        if (isNumber(char)) {
+            tokens.push({ string: char, type: 'number' })
+            i++
+            continue
+        }
+        if (operators.includes(char)) {
+            tokens.push({ string: char, type: 'operator' })
+            i++
+            continue
+        }
+        if (brackets.includes(char)) {
+            tokens.push({ string: char, type: 'bracket' })
+            i++
+            continue
+        }
+        tokens.push({ string: char, type: 'normal' })
+        i++
+    }
+
+    return tokens
+}
+
+export function lex(code) {
+    return highlight(optimize(tokenize(code)))
+}
---
+++ test.js
@@ -0,0 +1,51 @@
+import { lex, tokenize, optimize } from './index.js'
+import test from 'ava'
+
+// optimizing and tokenizing tests
+test('empty string', t => t.deepEqual(optimize(tokenize('')), []))
+test('chicken wing', t =>
+    t.deepEqual(optimize(tokenize('function chickenWing() {}')), [
+        { string: 'function', type: 'declaration' },
+        { string: ' chickenWing', type: 'normal' },
+        { string: '(', type: 'bracket' },
+        { string: ')', type: 'bracket' },
+        { string: ' ', type: 'normal' },
+        { string: '{', type: 'bracket' },
+        { string: '}', type: 'bracket' },
+    ]))
+test('string addition', t =>
+    t.deepEqual(optimize(tokenize('"bruh\'" + `hello sir`')), [
+        { string: `bruh'`, type: 'string', quote: '"' },
+        { string: ' ', type: 'normal' },
+        { string: '+', type: 'operator' },
+        { string: ' ', type: 'normal' },
+        { string: 'hello sir', type: 'string', quote: '`' },
+    ]))
+test('declaration in var name', t =>
+    t.deepEqual(optimize(tokenize(`const hellofunction = 'hola'`)), [
+        { string: 'const', type: 'declaration' },
+        { string: ' hellofunction ', type: 'normal' },
+        { string: '=', type: 'operator' },
+        { string: ' ', type: 'normal' },
+        { string: 'hola', type: 'string', quote: `'` },
+    ]))
+test('fizz buzz', t => {
+    t.deepEqual(optimize(tokenize(`function hola() {\n\n\n}`)), [
+        { string: 'function', type: 'declaration' },
+        { string: ' hola', type: 'normal' },
+        { string: '(', type: 'bracket' },
+        { string: ')', type: 'bracket' },
+        { string: ' ', type: 'normal' },
+        { string: '{', type: 'bracket' },
+        { string: `\n\n\n`, type: 'normal' },
+        { string: '}', type: 'bracket' },
+    ])
+})
+
+// complete lexing tests
+test('empty string 2', t => t.deepEqual(lex(''), ''))
+test('chicken wing 2', t =>
+    t.deepEqual(
+        lex('function chickenWing() {}'),
+        '<font color="cornflowerblue">function</font> chickenWing<font color="red">(</font><font color="red">)</font> <font color="red">{</font><font color="red">}</font>'
+    ))
