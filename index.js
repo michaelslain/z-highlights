32e75fe53ec97c27f6e3dc42b2f39386570f983d
michaelslain <misha.slain@icloud.com>
2020-12-30 12:12:50 -0800

    Added missing operator

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const operators = ['+', '-', '?', '*', '%', '/', '<', '>', '=', '!']
+const operators = ['+', '-', '?', '*', '%', '/', '<', '>', '=', '!', ':']
 const brackets = ['(', ')', '[', ']', '{', '}']
 const quotes = ['"', `'`, '`']
 const declarations = [
